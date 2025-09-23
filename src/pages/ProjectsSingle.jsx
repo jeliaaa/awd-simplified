e0c945ea59e32f1c80fc869d2b6e226ec54cc189
jeliaaa <julia.aleqsandre@gmail.com>
2025-09-23 12:47:51 +0400

    a

--- src/pages/ProjectsSingle.jsx
+++ src/pages/ProjectsSingle.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import { useApiStore } from '../store/apiStore';
 import { useParams } from 'react-router-dom';
+import TTSText from '../components/TTSText';
 
 const ProjectsSingle = () => {
     const { loading, projects, fetchProjects } = useApiStore();
-    const {year} = useParams()
+    const { year } = useParams()
     useEffect(() => {
         fetchProjects(year);
     }, [fetchProjects, year]);
@@ -18,7 +19,16 @@ const ProjectsSingle = () => {
 
 
     return (
-        <div>ProjectsSingle</div>
+        <div className='w-full'>
+            {projects.map((project) => (
+                <div className='w-full border-y-2 py-5' key={project.id}>
+                    <h1><TTSText content={project.name} /></h1>
+                    <h2><TTSText content={project.location} /></h2>
+                    <h3><TTSText content={project.date} /></h3>
+                    <div className='w-full' dangerouslySetInnerHTML={{__html : project.description || ""}}></div>
+                </div>
+            ))}
+        </div>
     )
 }
 
